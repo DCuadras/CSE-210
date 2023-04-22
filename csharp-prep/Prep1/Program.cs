3807022c5c5f69907a1aaf2fc83f9125f6fd2401
DCuadras <dcuadras@byui.edu>
2023-04-22 13:54:02 -0600

    final_prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your last name?");
-        string lastname = Console.ReadLine();
+        Console.WriteLine("What is your first name? ");
         string firstname = Console.ReadLine();
-        Console.WriteLine(lastname,firstname);
+        Console.WriteLine("What is your last name? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
 }
